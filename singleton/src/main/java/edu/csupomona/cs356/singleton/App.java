d43707bfa052e09de9ceff6ce6168db0c50f4827
Yu Sun <yu.sun.cs@gmail.com>
2014-10-28 09:24:38 -0700

    Clean up

--- singleton/src/main/java/edu/csupomona/cs356/singleton/App.java
+++ singleton/src/main/java/edu/csupomona/cs356/singleton/App.java
@@ -4,10 +4,5 @@ package edu.csupomona.cs356.singleton;
 public class App {
     public static void main( String[] args ) {
     	
-    		  IClickerService.getInstance("mobile");
-    		  
-    		  IClickerService.getInstance().setCourseName("CS356");
-    		  System.out.println(IClickerService.getInstance().getCourseName());
-    	
     }
 }
