435ab6746bd2714d1d4b63eb23a3e6689d57810c
Yu Sun <yu.sun.cs@gmail.com>
2014-11-14 09:36:55 -0800

    Observer broadcasting

---
+++ BroadCastDemo/app/src/main/java/cs356/csupomona/edu/broadcastdemo/ConnectivityReceiver.java
@@ -0,0 +1,37 @@
+package cs356.csupomona.edu.broadcastdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+import android.widget.Toast;
+
+public class ConnectivityReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Internet Connection Status: "
+                + haveNetworkConnection(context), Toast.LENGTH_LONG).show();
+    }
+
+
+    private boolean haveNetworkConnection(Context context) {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager)   context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+    }
+
+}
